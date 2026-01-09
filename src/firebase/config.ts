fc025fdb85ff7cee0715b8c84a04d0e102aa4b63
Stavo Midnite <stavogm@gmail.com>
2026-01-09 20:24:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1199156663-d3719",
+  "appId": "1:324319516768:web:957133f5aad96f412d8b05",
+  "apiKey": "AIzaSyA9RC6z-5NsE0LRngt5E_EgedlngLXeWhA",
+  "authDomain": "studio-1199156663-d3719.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "324319516768"
+};
