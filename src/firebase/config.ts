ae118f3a79e74fbbadead6a09df8baa333162a8f
Stavo Midnite <stavogm@gmail.com>
2026-01-11 00:01:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-1199156663-d3719",
   "appId": "1:324319516768:web:957133f5aad96f412d8b05",
+  "storageBucket": "studio-1199156663-d3719.firebasestorage.app",
   "apiKey": "AIzaSyA9RC6z-5NsE0LRngt5E_EgedlngLXeWhA",
   "authDomain": "studio-1199156663-d3719.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "324319516768"
 };
